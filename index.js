f7902107f1bd3af95872bd3b21b6f1a6d395c837
okputadora <okputadora@gmail.com>
2019-02-23 13:09:48 -0500

    scrape all

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const nightmare = Nightmare({ show: true });
 
 let selector = "tbody";
 
+let counter = 0;
+
 let dataTemplate = {
   genus: "",
   species: "",
@@ -23,28 +25,38 @@ let dataTemplate = {
 };
 
 let newTree = {};
-function recursiveScrape(newTree) {
+let index = 1;
+let pageIndex = 1;
+function recursiveScrape(index, pageIndex) {
   nightmare
     .goto("http://www.pabigtrees.com/view_tree.aspx")
-    .evaluate(selector => {
-      let list = document.querySelector(selector).children;
-      let index = 0;
-      let cellIndex = 0;
-      for (let item of list) {
-        if (index === 1) {
-          // console.log(item);
-          let cells = item.children;
-          cells.item(0).children.item(0).className = "elToClick";
+    .evaluate(pageIndex => {
+      let link = document.querySelector(
+        `[href="javascript:__doPostBack('ctl00$ContentPlaceHolder1$GridView1','Page$${2}')"]`
+      );
+      if (link) link.click();
+    }, pageIndex)
+    .wait(1000)
+    .evaluate(
+      context => {
+        let list = document.querySelector(context.selector).children;
+        let index = 0;
+        for (let item of list) {
+          if (index === context.index) {
+            // console.log(item);
+            let cells = item.children;
+            cells.item(0).children.item(0).className = "elToClick";
+          }
+          index++;
         }
-        index++;
-      }
-      window.county = "hello";
-      // get details page
-      // console.log("EL: ", el);
-      return list;
-    }, "tbody")
+        // get details page
+        // console.log("EL: ", el);
+        return list;
+      },
+      { selector: "tbody", index: index }
+    )
     .click(".elToClick")
-    .wait(2000)
+    .wait(1000)
     .evaluate(selector => {
       let treeDetails = {};
       let content = document.querySelector(selector).children;
@@ -85,36 +97,58 @@ function recursiveScrape(newTree) {
       treeDetails.points = m2.children.item(3).innerText;
 
       let addDetails = details.children.item(0).children.item(0);
-
-      treeDetails.address = addDetails.children
-        .item(1)
-        .children.item(1).innerText;
-      treeDetails.measuringCrew = addDetails.children
-        .item(2)
-        .children.item(1).innerText;
-      treeDetails.originalNominator = addDetails.children
-        .item(3)
-        .children.item(1).innerText;
-      treeDetails.comments = addDetails.children
-        .item(4)
-        .children.item(1).innerText;
-      treeDetails.measuringTechnique = addDetails.children
-        .item(5)
-        .children.item(1).innerText;
-      treeDetails.yearNominated = addDetails.children
-        .item(6)
-        .children.item(1).innerText;
-      treeDetails.lastMeasured = addDetails.children
-        .item(7)
-        .children.item(1).innerText;
+      let counter = 0;
+      for (let detail of addDetails.children) {
+        let detailKey;
+        let detailValue;
+        if (counter > 0) {
+          detailKey = detail.children
+            .item(0)
+            .innerText.split(" ")
+            .join("");
+          detailValue = detail.children.item(1).innerText;
+          treeDetails[detailKey] = detailValue;
+        }
+        counter++;
+      }
+      // treeDetails.address = addDetails.children
+      //   .item(1)
+      //   .children.item(1).innerText;
+      // treeDetails.measuringCrew = addDetails.children
+      //   .item(2)
+      //   .children.item(1).innerText;
+      // treeDetails.originalNominator = addDetails.children
+      //   .item(3)
+      //   .children.item(1).innerText;
+      // treeDetails.comments = addDetails.children
+      //   .item(4)
+      //   .children.item(1).innerText;
+      // treeDetails.measuringTechnique = addDetails.children
+      //   .item(5)
+      //   .children.item(1).innerText;
+      // treeDetails.yearNominated = addDetails.children
+      //   .item(6)
+      //   .children.item(1).innerText;
+      // treeDetails.lastMeasured = addDetails.children
+      //   .item(7)
+      //   .children.item(1).innerText;
 
       return treeDetails;
       // return treeMeasurements;
     }, "#main")
-    .then(tree => {})
+    .then(tree => {
+      console.log("page: ", pageIndex);
+      console.log("tree: ", index);
+      index += 1;
+      if (index > 20) {
+        pageIndex += 1;
+        index = 1;
+      }
+      recursiveScrape(index, pageIndex);
+    })
     .catch(error => {
       console.error("Search failed:", error);
     });
 }
 
-recursiveScrape(newTree);
+recursiveScrape(index, pageIndex);
